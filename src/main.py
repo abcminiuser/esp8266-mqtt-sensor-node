13238d4cc1e6a66c340f46fce6bee4fd6d22a1a2
Dean Camera <dean@fourwalledcubicle.com>
2017-12-26 20:49:48 +1100

    Clean up WiFi and MQTT connection code.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,18 @@
+import network
 import machine
 import ubinascii
 import time
+from umqtt.robust import MQTTClient
 
 from config import CONFIG
 from sensors import SI7021
 
 
-def setup_wlan(ssid, passphrase):
-    import network
-
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, passphrase)
-
-    return wlan
-
-def setup_mqtt(host, port, device_id):
-    from umqtt.robust import MQTTClient
-
-    client = MQTTClient(device_id, host, port)
-    client.connect()
-
-    return client
-
-
 
 print("Connecting to '{}'...".format(CONFIG['wifi']['ssid']))
-wlan = setup_wlan(CONFIG['wifi']['ssid'], CONFIG['wifi']['passphrase'])
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(CONFIG['wifi']['ssid'], CONFIG['wifi']['passphrase'])
 while not wlan.isconnected():
     machine.idle()
 print("Connected to Wifi.")
@@ -36,13 +22,14 @@ if device_id is None:
     device_id = "esp8266_{}".format(ubinascii.hexlify(wlan.config('mac')).decode())
 
 print("Connecting to MQTT host, device ID '{}'...".format(device_id))
-mqtt = setup_mqtt(CONFIG['mqtt']['host'], CONFIG['mqtt']['port'], device_id)
+mqtt = MQTTClient(device_id, CONFIG['mqtt']['host'], CONFIG['mqtt']['port'])
+mqtt.connect()
 print("Connected to MQTT broker.")
 
 print("Scanning I2C devices...")
 i2c = machine.I2C(sda=machine.Pin(12), scl=machine.Pin(14)) # D5/D6 on silkscreen
 found_i2c_devices = i2c.scan()
-print("Found devices at addresses: {}".format(found_i2c_devices))
+print("Found I2C devices at addresses: {}".format(' '.join(hex(x) for x in found_i2c_devices)))
 
 si7021 = SI7021.SI7021(0x40, i2c)
 
